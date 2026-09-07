21b303c53c6264215cfb09041dfd746017eebdd2
DrZerpentix <wolfrobot005@gmail.com>
2026-09-07 00:27:21 -0400

    Modify site description and update species/rarity options
    Updated the site description and modified species and rarity options.

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ let charadex = {};
 charadex.site = {
   title: "Varzeth Hollow",
   url: "https://drzerpentix.github.com/vharzeth-hollow",
-  description: `A tool for organizing small ARPGs and species.`
+  description: `18+ Space/angel related species.`
 }
 
 /* ==================================================================== */
@@ -44,8 +44,8 @@ charadex.sheet = {
 
     designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
     statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
-    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
-    species: ['All', 'Dog', 'Cat', 'Bunny'],
+    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Legendary', 'Mutation'],
+    species: ['All', 'Kyravials'],
     itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
     traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
 
